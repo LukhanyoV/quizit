9266fa0b9cd6f932402ab9bd2416ceb992e9aaf1
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2022-11-29 16:49:15 +0200

    use endpoints

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
+app.use("/api", require("./routes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`App running at http://localhost:${PORT}`));
