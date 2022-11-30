4034202524c9c0e0c726e6e891736eb647157579
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2022-11-30 13:56:10 +0200

    created table scripts

---
+++ sql/002.data.sql
@@ -0,0 +1,4 @@
+-- insert modes
+INSERT INTO modes (mode, start_num, end_num) VALUES ('easy', 1, 10);
+INSERT INTO modes (mode, start_num, end_num) VALUES ('medium', 11, 20);
+INSERT INTO modes (mode, start_num, end_num) VALUES ('hard', 21, 30);
