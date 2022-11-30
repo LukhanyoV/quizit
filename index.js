79a3f89194da2387f955ceea72dfaafe0cedf312
Yonela-Johannes <johannesyonela@gmail.com>
2022-11-30 12:01:13 +0200

    randomize questions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+app.use(require("cors")());
 app.use(express.static("public"));
 app.use("/api", require("./routes"));
 
---
+++ public/main.js
@@ -0,0 +1,27 @@
+// make references to the DOM elements
+const $ = document;
+const questionElement = $.querySelector(".questions");
+const answerRight = $.querySelector(".right");
+const answerLeft = $.querySelector(".left");
+
+// get the question data from API
+const URL = "http://localhost:5000/api/generate";
+
+// get the data from the API
+(async () => {
+    const results = await axios.get(URL);
+    const questionObj = results.data.data;
+    console.log("ho ho ho", results.data);
+    
+
+    // put the question in the html
+    questionElement.innerHTML = `${questionObj.question} = ?`;
+    const randomNum = Math.floor(Math.random() * 10); // will use this for deciding where to show option
+    if(randomNum % 2 === 0){
+            answerRight.innerHTML = `<span class="name">${questionObj.correct}</span>`;
+            answerLeft.innerHTML = `<span class="name">${questionObj.incorrect}</span>`;
+    } else {
+        answerRight.innerHTML = `<span class="name">${questionObj.incorrect}</span>`;
+        answerLeft.innerHTML = `<span class="name">${questionObj.correct}</span>`;
+    }
+})()
